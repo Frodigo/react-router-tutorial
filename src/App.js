0492a826f8047a48161ccb87e535b9edc280cea6
Marcin Kwiatkowski <contact@frodigo.com>
2021-12-12 13:09:52 +0100

    Add Navigation and links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { Navbar, Container, Nav } from 'react-bootstrap'
 
 export const App = () => {
   return <>
-    <h1>Hello, hello, hello</h1>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand>
+          <NavLink to="/" style={{textDecoration: 'none', color: 'inherit'}}>Your account</NavLink>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink to="/address" className="nav-link">Address book</NavLink>
+            <NavLink to="/orders" className="nav-link">Orders</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <Container className="mt-3">
+      <h1>Hello, hello, hello</h1>
+    </Container>
   </>
 }
 
