3157af6cfe8db2f5354a0fbde9029b1073658844
Marcin Kwiatkowski <contact@frodigo.com>
2021-12-17 06:45:01 +0100

    Add first route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { Routes, BrowserRouter, Route} from "react-router-dom";
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <Routes>
+            <Route path="/" element={<App />} />
+        </Routes>
     </BrowserRouter>,
     document.getElementById('root')
 );
